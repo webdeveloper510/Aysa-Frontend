a785663e6fde0447e3ec5bcc79cb41302bf7f852
NehaDesigner0118 <garg.neha0118@gmail.com>
2025-09-24 13:14:45 +0530

    changes the text tab 2

--- src/components/TabTwo.jsx
+++ src/components/TabTwo.jsx
@@ -532,8 +532,9 @@ export const TabTwo = () => {
                   </Grid>
 
                   <Typography variant="h6" mt="10px" align="left">
-                    CEO {row.ceo_name} made {row.ceo_total_compensation} vs. the
-                    average worker making {row.worker_salary}.That’s{" "}
+                    CEO {row.ceo_name} made an estimate of{" "}
+                    {row.ceo_total_compensation} vs. the average worker making{" "}
+                    {row.worker_salary}.That’s{" "}
                     {row["pay_ration"]?.toLowerCase().replace("x", "")} times
                     more.
                   </Typography>

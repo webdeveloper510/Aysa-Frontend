3805f423676cf65f6ebef20a9eaeaca1ced1787b
webdeveloper510 <developerweb6@gmail.com>
2025-09-29 16:34:19 +0530

    dashboard changes c1195

--- src/CustomHooks/useCardscount.js
+++ src/CustomHooks/useCardscount.js
@@ -26,19 +26,21 @@ export function useCardscount() {
 
         const today = new Date().toISOString().split("T")[0];
 
-        const count = await axios.get(
-          `${process.env.REACT_APP_API_URL}/get-visitor?date_str=${today}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        setTodaysVisitors(count.data.total_visit_count);
-
         setTodaysSearchedProducts(res?.data?.data?.length || 0);
       } catch (err) {
         console.error(err);
+      }
+
+      try {
+        const count = await axios.get(
+          `${process.env.REACT_APP_API_URL}/get-visitor/`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        setTodaysVisitors(count.data.total_visit_count);
+      } catch (err) {
+        console.error("Error fetching visitors:", err);
       }
     }
 
